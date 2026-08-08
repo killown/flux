f1ad9534af58796489c4638c12ece340a7a25917
Thiago <killown.matrix@gmail.com>
2026-08-08 12:10:15 -0300

    test(services,utils): add unit tests for LUKS service, config parsing, and MIME handling

--- tests/services.rs
+++ tests/services.rs
@@ -2,6 +2,7 @@ mod services {
     mod archive_services_test;
     mod delete_ops_test;
     mod file_loader_test;
+    mod luks_test;
     mod network_services_test;
     mod search_ops_test;
     mod state_manager_test;
